cf6aa63f15fdb451c37a0f0d85c94a1c1f85e22a
kyuhodad <kyuhodad@gmail.com>
2015-09-26 07:59:55 -0400

    Added a general testing method for simple rules

--- js/tests/spec/zombieTranslatorSpec.js
+++ js/tests/spec/zombieTranslatorSpec.js
@@ -92,6 +92,93 @@ define(['zombieTranslator'], function(ZombieTranslator){
       }
     });
 
+
+    // Capitalize the first character of sentences.
+    describe('Rule4: <e or E> --> <rr>', function () {
+      it ('should work with simple words.', function () {
+        var testStrings = ["Serve", "enter", "sleeve"];
+        for (var i=0; i<testStrings.length; i++) {
+          expect(checkSimpleReplace(testStrings[i], /e|E/g, "rr", "@")).toBe(true);
+        }
+      });
+      it ('should work with a sentece.', function () {
+        var testStrings = ["He needs to complete his homework.", "Enter the house."];
+        for (var i=0; i<testStrings.length; i++) {
+          expect(checkSimpleReplace(testStrings[i], /e|E/g, "rr", "@")).toBe(true);
+        }
+      });
+      it ('should work with multiple sentences.', function () {
+        var testString = "He needs to complete his homework. Enter the house.";
+        expect(checkSimpleReplace(testString, /e|E/g, "rr", "@")).toBe(true);
+      });
+    });
+
+    // ------------------------------------------------------------------------------------
+    // Apply the rule replacing chToReplace with strReplaceWith to given string(strToTest)
+    // Returns true if the rule is working.
+    //  strToTest: String to test
+    //  chToReplace: A character being replaced (it could be RegExp object)
+    //  strReplaceWith: A string to replace with.
+    // ------------------------------------------------------------------------------------
+    function checkSimpleReplace (strToTest, chToReplace, strReplaceWith, convCh) {
+
+      if (convCh === undefined) convCh = "@";
+      var regexpToReplace;
+      if      (typeof chToReplace === "string") regexpToReplace = new RegExp(chToReplace, "g");
+      else if (chToReplace instanceof RegExp) regexpToReplace = chToReplace;
+      else return false;
+
+      // Replace the character to test (chToReplace) with special character(convCh)
+      // NOTE: Assuming convCh doesn't appear in strToTest and it should be one of
+      //       non-alpha-numeric character.
+      // EX)  strToTest = "Enter the house."
+      //      chToReplace = /e|E/g
+      //      convCh = "_"
+      //      Then, strConverted would be "_nt_r th_ hous_."
+      strToTest = strToTest.trim();
+      var strConverted = strToTest.replace(regexpToReplace, convCh);
+
+      var original = transletor.translate(strToTest);
+      var modified = transletor.translate(strConverted);
+
+      var capStrReplaceWith = strReplaceWith.charAt(0).toUpperCase() + strReplaceWith.substring(1);
+      var beginWithCapitalized = (strToTest.search(regexpToReplace) === 0);
+
+      var currentStr = original;
+      var splitModified = modified.split(convCh);
+      for (var id=0; id<splitModified.length; id++) {
+        var strToken = splitModified[id];
+
+        var needCapitalized = false;
+        if (strToken.length > 0) {
+          // strToken should appear the beginning of currentStr string.
+          var pos = currentStr.search(strToken);
+          if (pos != 0) return false;
+
+          // If strToken is the end of a sentece, next character needs to be replace with
+          // capitalized.
+          needCapitalized = /[\.|!|\?]\s*$/.test(strToken);
+          // needCapitalized = strToken.test(/[\.|!|\?]\s*$/);
+        } else if (id === 0) {
+          // Check to see if the strToTest starts with chToReplace.
+          needCapitalized = beginWithCapitalized;
+        }
+
+        // Extract remaining string followed by the strToken.
+        var nextStr = currentStr.substring(strToken.length);
+        if (nextStr.length === 0) break;
+
+        // The nextStr should start with strReplaceWith.
+        var searchStr = needCapitalized ? capStrReplaceWith : strReplaceWith;
+        var posReplaceWith = nextStr.search(searchStr);
+        if (posReplaceWith != 0) return false;
+
+        currentStr = nextStr.substring(strReplaceWith.length);
+        if (currentStr === undefined || currentStr.length == 0) break;
+      }
+      return true;
+    }
+
   });
 
 
